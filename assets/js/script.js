7278595ac0a6f10e34b87ea58f570c3d69edc2ac
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-05-02 20:09:02 +0200

    refacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,10 @@ function calcAge() {
 	var ageEnAnnees = anneeActuel - anneeDeNaissance;
 	var joursTerrestre = ageEnAnnees * 365;
 	anneeMarcienne = joursTerrestre / 687;
+	for (var i = 0; i < 9; i++) {
+		document.getElementById(i).innerHTML = (joursTerrestre / joursRevolutionPlanete[i]).toFixed(2).replace('.', ',');
+	}
 
-	document.getElementById('0').innerHTML = (joursTerrestre / 88).toFixed(2).replace('.', ',');
-	document.getElementById('1').innerHTML = (joursTerrestre / 255).toFixed(2).replace('.', ',');
 	document.getElementById('2').innerHTML = ageEnAnnees;
-	document.getElementById('3').innerHTML = (joursTerrestre / 687).toFixed(2).replace('.', ',');
-	document.getElementById('4').innerHTML = (joursTerrestre / 4329).toFixed(2).replace('.', ',');
-	document.getElementById('5').innerHTML = (joursTerrestre / 10751).toFixed(2).replace('.', ',');
-	document.getElementById('6').innerHTML = (joursTerrestre / 30664).toFixed(2).replace('.', ',');
-	document.getElementById('7').innerHTML = (joursTerrestre / 60148).toFixed(2).replace('.', ',');
-	document.getElementById('8').innerHTML = (joursTerrestre / 90520).toFixed(2).replace('.', ',');
 
 }
